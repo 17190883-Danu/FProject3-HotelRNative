508e9e7e94cdf1c169519f811929e4cbc7863b33
17190883-Danu <danutriprasetyo138@gmail.com>
2022-11-20 16:20:00 +0700

    ui terbaru

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from './navigation/tabs';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const App = () => {
   return(
-    <NavigationContainer>
-      <Tabs/>
-  </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+          <Tabs/>
+      </NavigationContainer>
+    </SafeAreaProvider>
+    
   );
 }
 
--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,17 +1,20 @@
 // import React from "react";
 import React, {useState, useEffect} from 'react';
 import { View, Text, Button, StyleSheet, Image, TextInput } from "react-native";
+import { Button as ButtonB } from '@rneui/themed';
 
 const HomeScreen = ({navigation}) => {
     const [search, setSearch] = useState('');
     return (
         <View style={styles.container}>
+
+            {/* SEARCH */}
             <Image 
                 source={require('hotelfp/assets/icons/home.png')}
                 style={{
                     width: 25,
                     height: 25,
-                    alignItems: 'row',
+                    // alignItems: 'row',
                 }}
             />
             <TextInput
@@ -21,6 +24,55 @@ const HomeScreen = ({navigation}) => {
                 underlineColorAndroid="transparent"
                 placeholder="Search Here"
             />
+
+            {/* DATE/KALENDER */}
+            <Image 
+                source={require('hotelfp/assets/icons/kalender.png')}
+                resizeMode='contain'
+                style={{
+                    width: 25,
+                    height: 25,
+                    // tintColor: focused ? '#e32f45' : '#748c94',
+                }}
+            />
+            <Text style={{fontSize: 12}}>Check-in Date</Text>
+
+            <Image 
+                source={require('hotelfp/assets/icons/kalender.png')}
+                resizeMode='contain'
+                style={{
+                    width: 25,
+                    height: 25,
+                    // tintColor: focused ? '#e32f45' : '#748c94',
+                }}
+            />
+            <Text style={{fontSize: 12}}>Check-Out Date</Text>
+
+            {/* TAMU */}
+            <Image 
+                source={require('hotelfp/assets/icons/profil.png')}
+                resizeMode='contain'
+                style={{
+                    width: 25,
+                    height: 25,
+                    // tintColor: focused ? '#e32f45' : '#748c94',
+                }}
+            />
+            <Text style={{fontSize: 12}}>Guest</Text>
+
+            {/* BUTTON SEARCH */}
+            <ButtonB
+                // ViewComponent={Search} // Don't forget this!
+                // linearGradientProps={{
+                //     colors: ["#FF9800", "#F44336"],
+                //     start: { x: 0, y: 0.5 },
+                //     end: { x: 1, y: 0.5 },
+                // }}
+                >
+                Search
+            </ButtonB>
+
+            
         </View>
     );
 };
